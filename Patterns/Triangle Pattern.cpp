9f3a252f048518e221e3f01cf5d49d3cacba89a0
Khushi-patil-2004 <162123340+Khushi-patil-2004@users.noreply.github.com>
2025-06-27 12:16:20 +0530

    Create Triangle Pattern.cpp

---
+++ Patterns/Triangle Pattern.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+void trianglepattern(int n)
+{
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+           cout <<"*";
+        }
+         cout << endl;
+    }
+}
+int main() {
+ int n;
+ cout<<"enter the number:"<<endl;
+ cin>>n;
+ cout<<"Triangle Pattern"<<endl;
+ trianglepattern(n);
+    return 0;
+}
