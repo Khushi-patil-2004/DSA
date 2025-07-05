b54d899f9357a4226ff57fe8545e16711ebbb2e8
Khushi-patil-2004 <162123340+Khushi-patil-2004@users.noreply.github.com>
2025-07-05 20:42:41 +0530

    Create BinaryTree Traversal.cpp

---
+++ Tree/BinaryTree Traversal.cpp
@@ -0,0 +1,94 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+class Tree {
+    Node* root;
+
+    Node* buildtree(Node* root, int val) {
+        if (root == NULL) {
+            return new Node(val);
+        }
+        if (val < root->data) {
+            root->left = buildtree(root->left, val);
+        } else {
+            root->right = buildtree(root->right, val);
+        }
+        return root;
+    }
+
+    void inorder(Node* node) {
+        if (node == NULL) return;
+        inorder(node->left);
+        cout << node->data << " ";
+        inorder(node->right);
+    }
+
+    void preorder(Node* node) {
+        if (node == NULL) return;
+        cout << node->data << " ";
+        preorder(node->left);
+        preorder(node->right);
+    }
+
+    void postorder(Node* node) {
+        if (node == NULL) return;
+        postorder(node->left);
+        postorder(node->right);
+        cout << node->data << " ";
+    }
+
+public:
+    Tree() {
+        root = NULL;
+    }
+
+    void insertNode(int val) {
+        root = buildtree(root, val);
+    }
+
+    void displayInorder() {
+        cout << "Inorder Traversal: ";
+        inorder(root);
+        cout << endl;
+    }
+
+    void displayPreorder() {
+        cout << "Preorder Traversal: ";
+        preorder(root);
+        cout << endl;
+    }
+
+    void displayPostorder() {
+        cout << "Postorder Traversal: ";
+        postorder(root);
+        cout << endl;
+    }
+};
+
+int main() {
+    Tree t;
+    t.insertNode(50);
+    t.insertNode(30);
+    t.insertNode(70);
+    t.insertNode(20);
+    t.insertNode(40);
+    t.insertNode(60);
+    t.insertNode(80);
+
+    t.displayInorder();    
+    t.displayPreorder();   
+    t.displayPostorder();  
+
+    return 0;
+}
