f9d6bdc948ffda59975c94a1e76a97e94cc28d1a
Khushi Patil <162123340+Khushi-patil-2004@users.noreply.github.com>
2025-09-05 13:22:29 +0530

    Create Hollow_Rectangle_Pattern.cpp

---
+++ Patterns/Hollow Rectangle Pattern.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+void Pattern(int n)
+{
+    for (int i = 0; i < n; i++)   
+    {
+        for (int j = 0; j < n; j++)   
+        {
+            if (i == 0 || j == 0 || i == n - 1 || j == n - 1)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+   }
+}
+int main()
+{
+    int n;
+    cout << "Enter size: ";
+    cin >> n;
+    Pattern(n);
+    return 0;
+}
