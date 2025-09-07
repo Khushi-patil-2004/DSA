7a4f11fb4ed24ed12aeaf2a6f045f7f96a4e1b39
Khushi Patil <162123340+Khushi-patil-2004@users.noreply.github.com>
2025-09-07 13:58:27 +0530

    Update longest_common_prefix.cpp

--- String/longest_common_prefix.cpp
+++ String/longest_common_prefix.cpp
@@ -1,43 +1,43 @@
 //Write a function to find the longest common prefix string amongst an array of strings.
 //If there is no common prefix, return an empty string "".
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
+#include <algorithm>
 using namespace std;
 
-string longestCommonPrefix(string str[],int n)
-{
-    if(n == 0)
-    {
-        return " ";
+string longestCommonPrefix(string str[], int n) {
+    if (n == 0) return "";
+
+    // Step 1: Sort the array of strings
+    sort(str, str + n);
+
+    // Step 2: Compare first and last string
+    string first = str[0];
+    string last = str[n - 1];
+
+    int i = 0;
+    while (i < first.length() && i < last.length() && first[i] == last[i]) {
+        i++;
     }
-    string prefix = str[0];
-    
-    for(int i=1;i<n;i++)
-    {
-        int j=0;
-       while (j < prefix.length() && j < str[i].length() && prefix[j] == str[i][j]) {
-            j++;
-        }
-        prefix = prefix.substr(0,j);
-         if (prefix == "") {
-            return ""; 
-         }
-    }
-    return prefix;
-    
+
+    // Step 3: Common prefix is substring of first (or last) up to i
+    return first.substr(0, i);
 }
-int main()
-{     int n;
-  cout << "Enter the number of strings: ";
-   cin >> n;
-   cin.ignore();
+
+int main() {
+    int n;
+    cout << "Enter number of strings: ";
+    cin >> n;
+    cin.ignore();
+
     string str[n];
-    cout<<"enter the string"<<endl;
-     for (int i = 0; i < n; i++) {
+    cout << "Enter the strings:" << endl;
+    for (int i = 0; i < n; i++) {
         getline(cin, str[i]);
     }
-     string result = longestCommonPrefix(str, n);
+
+    string result = longestCommonPrefix(str, n);
     cout << "Longest Common Prefix: " << result << endl;
-    
+
     return 0;
 }
