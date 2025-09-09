f156c7ca944d17e95d402183514851c260f62d55
Khushi Patil <162123340+Khushi-patil-2004@users.noreply.github.com>
2025-09-09 18:02:41 +0530

    Create pattern3.cpp

---
+++ s_pattern/pattern3.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+void pattern(int n)
+{
+  for(int i=1;i<=n;i++)
+ {
+     for(int j=1;j<=i;j++)
+     {
+        cout<<j<<" ";   
+     }
+     cout<<endl;
+ }
+ }
+int main()
+{
+    int n;
+    cout<<"enter the number";
+    cin>>n;
+    cout<<"Pattern:"<<endl;
+    pattern(n);
+    return 0;
+}
+
+output:
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+1 2 3 4 5 6
