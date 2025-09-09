f5fb0046069392b6d7d7e9195da4badbce03523b
Khushi Patil <162123340+Khushi-patil-2004@users.noreply.github.com>
2025-09-09 19:24:29 +0530

    Create pattern7.cpp

---
+++ s_pattern/pattern7.cpp
@@ -0,0 +1,41 @@
+#include<iostream>                                 
+using namespace std;
+void pattern(int n)
+{
+ for(int i=0;i<n;i++)
+ {
+     //space
+     for(int j =0; j<i; j++)
+     {
+         cout<<" ";
+     }
+     //star
+     for(int j=0;j< 2*n - (2*i+1);j++)
+     {
+         cout<<"*";
+     }
+     //space
+    for(int j =0; j<i; j++)
+     {
+         cout<<" ";
+     }
+     cout<<endl;
+ }
+ }
+int main()
+{
+    int n;
+    cout<<"enter the number";
+    cin>>n;
+    cout<<"Pattern:"<<endl;
+    pattern(n);
+    return 0;
+}
+
+output:
+***********
+ *********
+  *******
+   ***** 
+    ***    
+     *
